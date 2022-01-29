478c3819ae2b8151224c613f7a5d5785b92b1a26
Lauren McCall <lmmccolloch@gmail.com>
2022-01-29 18:11:19 -0500

    View by Gender now eliminates null name fields in output

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -25,34 +26,84 @@ namespace ApiClient
             Console.WriteLine("[V]iew all available data ");
             Console.WriteLine("[G]ender");
             Console.WriteLine("[C]ulture ");
-            Console.WriteLine("[A]nyone alive still? ");
+            Console.WriteLine("[A]nyone still alive? ");
             Console.WriteLine("");
         }
 
         static async Task Main(string[] args)
         {
-            await ViewByGender();
+            var keepGoing = true;
+
+            DisplayMenu();
+            while (keepGoing)
+            {
+                DisplayMenu();
+                var choice = Console.ReadLine().ToUpper();
+                Console.WriteLine("");
+
+
+                switch (choice)
+                {
+                    case "V":
+                        await ViewAllCharacterData();
+                        break;
+                    case "G":
+                        await ViewByGender();
+                        break;
+                    case "C":
+                        break;
+                    case "A":
+                        break;
+                }
+            }
         }
 
         private static async Task ViewByGender()
         {
             var client = new HttpClient();
-
             var genderSearch = PromptForString("Please type: Female or Male \n");
 
             var url = $"https://anapioficeandfire.com/api/characters?gender={genderSearch}";
             var responseAsStream = await client.GetStreamAsync(url);
 
             var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
-            // Console.WriteLine(responseAsStream);
+            var onlyFemales = characters.Where(character => character.Name != "");
+            var onlyMales = characters.Where(character => character.Name != "");
 
             var table = new ConsoleTable("Name", "Gender", "Culture", "Born");
 
+            if (genderSearch == "Female")
+            {
+                foreach (var person in onlyFemales)
+                {
+                    table.AddRow(person.Name, person.Gender, person.Culture, person.Born);
+                }
+            }
+            else if (genderSearch == "Male")
+            {
+                foreach (var person in onlyMales)
+                {
+                    table.AddRow(person.Name, person.Gender, person.Culture, person.Born);
+                }
+            }
+            table.Write();
+        }
+
+        private static async Task ViewAllCharacterData()
+        {
+            var client = new HttpClient();
+            // var genderSearch = PromptForString("Please type: Female or Male \n");
+
+            var url = $"https://anapioficeandfire.com/api/characters";
+            var responseAsStream = await client.GetStreamAsync(url);
+
+            var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
+            var table = new ConsoleTable("Name", "Gender", "Culture", "Born", "Died");
+
             foreach (var person in characters)
             {
-                table.AddRow(person.Name, person.Gender, person.Culture, person.Born);
+                table.AddRow(person.Name, person.Gender, person.Culture, person.Born, person.Died);
             }
-
             table.Write();
         }
     }
