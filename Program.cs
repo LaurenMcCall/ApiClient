74c41efca1137859b4343ee0eff8ff55053dc288
Lauren McCall <lmmccolloch@gmail.com>
2022-01-29 19:47:08 -0500

    can search by name

--- Program.cs
+++ Program.cs
@@ -26,7 +26,8 @@ namespace ApiClient
             Console.WriteLine("[V]iew all available data ");
             Console.WriteLine("[G]ender");
             Console.WriteLine("[C]ulture ");
-            Console.WriteLine("[A]nyone still alive? ");
+            Console.WriteLine("[W]ho is with the gods now? ");
+            Console.WriteLine("[S]earch by name ");
             Console.WriteLine("[Q]uit...and tell me that thing about the night...");
             Console.WriteLine("");
         }
@@ -52,7 +53,11 @@ namespace ApiClient
                     case "C":
                         await ViewByCulture();
                         break;
-                    case "A":
+                    case "W":
+                        await ViewByIsAlive();
+                        break;
+                    case "S":
+                        await SearchByName();
                         break;
                     case "Q":
                         Console.WriteLine("Remember...The night is dark and full of terrors. ");
@@ -64,6 +69,27 @@ namespace ApiClient
                         break;
                 }
             }
+        }
+
+        private static async Task SearchByName()
+        {
+            var client = new HttpClient();
+            var nameSearch = PromptForString("Which character would you like to view? \n").ToUpper();
+
+            var url = $"https://anapioficeandfire.com/api/characters?name={nameSearch}&page=1&pageSize=50";
+            var responseAsStream = await client.GetStreamAsync(url);
+
+            var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
+            var removeNullNames = characters.Where(character => character.Name != "").OrderBy(character => character.Name);
+
+            var table = new ConsoleTable("Name", "Gender", "Culture", "Born");
+
+            foreach (var person in removeNullNames)
+            {
+                table.AddRow(person.Name, person.Gender, person.Culture, person.Born);
+            }
+
+            table.Write();
         }
 
         private static async Task ViewByGender()
@@ -83,11 +109,11 @@ namespace ApiClient
                 Console.WriteLine("That's not a valid selection. ");
             }
 
-            var url = $"https://anapioficeandfire.com/api/characters?gender={genderSearch}";
+            var url = $"https://anapioficeandfire.com/api/characters?gender={genderSearch}&page=1&pageSize=50";
             var responseAsStream = await client.GetStreamAsync(url);
 
             var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
-            var removeNullNames = characters.Where(character => character.Name != "");
+            var removeNullNames = characters.Where(character => character.Name != "").OrderBy(character => character.Name);
 
             var table = new ConsoleTable("Name", "Gender", "Culture", "Born");
 
@@ -102,15 +128,16 @@ namespace ApiClient
         private static async Task ViewAllCharacterData()
         {
             var client = new HttpClient();
-            // var genderSearch = PromptForString("Please type: Female or Male \n");
 
-            var url = $"https://anapioficeandfire.com/api/characters";
+            var url = $"https://www.anapioficeandfire.com/api/characters?page=1-43&pageSize=50";
             var responseAsStream = await client.GetStreamAsync(url);
 
             var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
+            var removeNullNames = characters.Where(character => character.Name != "").OrderBy(character => character.Name);
+
             var table = new ConsoleTable("Name", "Gender", "Culture", "Born", "Died");
 
-            foreach (var person in characters)
+            foreach (var person in removeNullNames)
             {
                 table.AddRow(person.Name, person.Gender, person.Culture, person.Born, person.Died);
             }
@@ -120,7 +147,42 @@ namespace ApiClient
         private static async Task ViewByCulture()
         {
             var client = new HttpClient();
-            var cultureSearch = PromptForString("Would you like to view characters from [W]esteros or [B]raavos? \n").ToUpper();
+            var cultureSearch = PromptForString("Please make a selection: \n[Q]artheen, [D]ornish, [N]orthmen, [I]ronborn, [W]esteros, [B]raavosi, [D]othraki or [V]alyrian \n").ToUpper();
+            cultureSearch = CulturesToSelect(cultureSearch);
+
+            var url = $"https://anapioficeandfire.com/api/characters?culture={cultureSearch}&page=1&pageSize=50";
+            var responseAsStream = await client.GetStreamAsync(url);
+
+            var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
+            var removeNullNames = characters.Where(character => character.Name != "").OrderBy(character => character.Name);
+
+            var table = new ConsoleTable("Name", "Culture", "Gender", "Born", "Died");
+
+            foreach (var person in removeNullNames)
+            {
+                table.AddRow(person.Name, person.Gender, person.Culture, person.Born, person.Died);
+            }
+            table.Write();
+        }
+
+        private static string CulturesToSelect(string cultureSearch)
+        {
+            if (cultureSearch == "Q")
+            {
+                cultureSearch = "Qartheen";
+            }
+            if (cultureSearch == "D")
+            {
+                cultureSearch = "Dornish";
+            }
+            if (cultureSearch == "I")
+            {
+                cultureSearch = "Ironborn";
+            }
+            if (cultureSearch == "N")
+            {
+                cultureSearch = "Northmen";
+            }
             if (cultureSearch == "W")
             {
                 cultureSearch = "Westeros";
@@ -129,18 +191,32 @@ namespace ApiClient
             {
                 cultureSearch = "Braavosi";
             }
+            else if (cultureSearch == "V")
+            {
+                cultureSearch = "Valyrian";
+            }
+            else if (cultureSearch == "D")
+            {
+                cultureSearch = "Dothraki";
+            }
             else
             {
                 Console.WriteLine("That's not a valid selection. ");
             }
+            return cultureSearch;
+        }
 
-            var url = $"https://anapioficeandfire.com/api/characters?culture={cultureSearch}";
+        private static async Task ViewByIsAlive()
+        {
+            var client = new HttpClient();
+
+            var url = $"https://anapioficeandfire.com/api/characters?isAlive={false}&page=1&pageSize=50";
             var responseAsStream = await client.GetStreamAsync(url);
 
             var characters = await JsonSerializer.DeserializeAsync<List<Characters>>(responseAsStream);
             var removeNullNames = characters.Where(character => character.Name != "").OrderBy(character => character.Name);
 
-            var table = new ConsoleTable("Name", "Culture", "Gender", "Born", "Died");
+            var table = new ConsoleTable("Name", "Gender", "Culture", "Born", "Died");
 
             foreach (var person in removeNullNames)
             {
